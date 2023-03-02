d0a89eb04589d7d0f2ae6cfddd2323c0ff38f941
rui0323 <rui19990323@icloud.com>
2023-03-02 13:43:51 +0000

    scss

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,9 @@
 class CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
   def show
      @customer = Customer.find(params[:id])
      @posts = @customer.posts.page(params[:page])
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :post_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
   end
-  resources :customers, only: [:show, :edit, :update]
+  resources :customers, only: [:show, :edit, :update, :index]
   get 'homes/top'
   get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
